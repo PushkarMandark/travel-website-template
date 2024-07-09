fa058576241cb7d85cb21e053011d143a48bdd3c
Sumit Kumar Ray <pushkar.meclpu@gmail.com>
2024-07-10 00:11:42 +0530

    css

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,43 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#fda720",
+        secondary: "#00aef3",
+        tertiary: "#f2f2f2",
+        accent: "#f06292",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      fontWeight: {
+        thin: 100,
+        extralight: 200,
+        light: 300,
+        normal: 400,
+        medium: 500,
+        semibold: 600,
+        bold: 700,
+        extrabold: 800,
+        black: 900,
+      },
+      fontStyle: {
+        normal: "normal",
+        italic: "italic",
+      },
+      spacing: {
+        72: "18rem",
+        84: "21rem",
+        96: "24rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+      boxShadow: {
+        xl: "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)",
+      },
+    },
   },
   plugins: [],
 };
